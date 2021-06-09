1cc1a3d36bf164cba6bd6cd7b70db32b8df0565b
User <User@DESKTOP-5IFU6NT>
2021-06-09 18:21:18 +0430

    Add services to Models.

---
+++ NovinTehran.Core/Models/Service.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Web.Mvc;
+
+namespace NovinTehran.Core.Models
+{
+    public class Service : IBaseEntity
+    {
+        public int Id { get; set; }
+        [Display(Name = "عنوان مقاله")]
+        [MaxLength(600,ErrorMessage = "{0} باید از 600 کارکتر کمتر باشد")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string Title { get; set; }
+
+        [Display(Name = "توضیح کوتاه")]
+        [DataType(DataType.MultilineText)]
+        [AllowHtml]
+        public string ShortDescription { get; set; }
+        [Display(Name = "توضیح")]
+        [DataType(DataType.MultilineText)]
+        [AllowHtml]
+        public string Description { get; set; }
+        public int ViewCount { get; set; }
+        [Display(Name = "تصویر")]
+        public string Image { get; set; }
+        public DateTime? AddedDate { get; set; }
+
+        public int? ServiceCategoryId { get; set; }
+        public ServiceCategory ServiceCategory { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<ServiceHeadLine> ServiceHeadLines { get; set; }
+
+        public ICollection<ServiceOrder> ServiceOrders { get; set; }
+
+        public string InsertUser { get; set; }
+        public DateTime? InsertDate { get; set; }
+        public string UpdateUser { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
