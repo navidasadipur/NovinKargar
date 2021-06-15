c18eb02e245a181abdea97712beb2f2970727886
User <User@DESKTOP-5IFU6NT>
2021-06-15 18:15:01 +0430

    copy and modify ServiceOrder from Invoice. views and controller and ServiceOrderViewModel.

---
+++ NovinTehran.Web/Areas/Admin/Controllers/ServiceOrdersController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NovinTehran.Core.Models;
+using NovinTehran.Core.Utility;
+using NovinTehran.Infrastructure.Repositories;
+using NovinTehran.Web.ViewModels;
+
+namespace NovinTehran.Web.Areas.Admin.Controllers
+{
+    public class ServiceOrdersController : Controller
+    {
+        private readonly ServiceOrdersRepository _repo;
+        private readonly GeoDivisionsRepository _GeoRepo;
+
+        public ServiceOrdersController(ServiceOrdersRepository repo, GeoDivisionsRepository geoRepo)
+        {
+            _repo = repo;
+            _GeoRepo = geoRepo;
+        }
+
+        // GET: Admin/Orders
+        public ActionResult Index()
+        {
+            var serviceOrders = _repo.GetServiceOrders();
+            var vm = new List<ServiceOrderTableViewModel>();
+            foreach (var serviceOrder in serviceOrders)
+            {
+               vm.Add(new ServiceOrderTableViewModel(serviceOrder)); 
+            }
+            return View(vm);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ServiceOrder serviceOrder = _repo.Get(id.Value);
+            if (serviceOrder == null)
+            {
+                return HttpNotFound();
+            }
+            //ViewBag.GeoDivisionId = new SelectList(_GeoRepo.GetGeoDivisionsByType((int)GeoDivisionType.State), "Id", "Title", serviceOrder.GeoDivisionId);
+            return View(serviceOrder);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ServiceOrder serviceOrder)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _repo.Update(serviceOrder);
+                return RedirectToAction("Index");
+            }
+            //ViewBag.GeoDivisionId = new SelectList(_GeoRepo.GetGeoDivisionsByType((int)GeoDivisionType.State), "Id", "Title", serviceOrder.GeoDivisionId);
+            return View(serviceOrder);
+        }
+
+        public ActionResult ViewServiceOrder(int serviceOrderId)
+        {
+            var vm = new ViewServiceOrderViewModel();
+            var serviceOrder = _repo.GetServiceOrder(serviceOrderId);
+            vm.ServiceOrder = serviceOrder;
+            vm.PersianDate = new PersianDateTime(serviceOrder.AddedDate).ToString();
+
+            //vm.OrderItems = new List<OrderItemWithMainFeatureViewModel>();
+
+            // Getting Order Item SubFeatures
+            //foreach (var serviceOrderItem in serviceOrder.OrderItems)
+            //{
+            //    var serviceOrderItemWithMainFeature = new OrderItemWithMainFeatureViewModel
+            //    {
+            //        OrderItem = serviceOrderItem, MainFeature = _repo.GetOrderItemsMainFeature(serviceOrderItem.Id)
+            //    };
+            //    vm.OrderItems.Add(serviceOrderItemWithMainFeature);
+
+            //}
+
+            return View(vm);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ServiceOrder serviceOrder = _repo.Get(id.Value);
+            if (serviceOrder == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PersianDate = new PersianDateTime(serviceOrder.AddedDate).ToString();
+            return PartialView(serviceOrder);
+        }
+
+        // POST: Admin/Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var serviceOrder = _repo.Get(id);
+            _repo.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ NovinTehran.Web/ViewModels/ServiceOrderViewModels.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using NovinTehran.Core.Models;
+
+namespace NovinTehran.Web.ViewModels
+{
+    public class ServiceOrderTableViewModel
+    {
+        public ServiceOrderTableViewModel()
+        {
+        }
+        public ServiceOrderTableViewModel(ServiceOrder serviceOrder)
+        {
+            this.ServiceOrder = serviceOrder;
+            this.PersianDate = new PersianDateTime(serviceOrder.AddedDate).ToString();
+            this.Customer = serviceOrder.CustomerFirstName;
+            this.Customer = "-";
+            if (serviceOrder.CustomerLastName != null)
+            {
+                this.Customer = serviceOrder.CustomerLastName;
+            }
+            else
+            {
+                if (serviceOrder.CustomerId != null)
+                {
+                    var user = serviceOrder.Customer.User;
+                    this.Customer = $"{user.FirstName} {user.LastName}";
+                }
+                else
+                {
+                    this.Customer = "-";
+                }
+            }
+        }
+        public ServiceOrder ServiceOrder { get; set; }
+        [Display(Name = "تاریخ ثبت")]
+        public string PersianDate { get; set; }
+
+        public string Customer { get; set; }
+
+        public string Phone { get; set; }
+    }
+
+    public class ViewServiceOrderViewModel
+    {
+        public ServiceOrder ServiceOrder { get; set; }
+        public string PersianDate { get; set; }
+        public List<ServiceOrderItemWithMainFeatureViewModel> ServiceOrderItems { get; set; }
+    }
+    public class ServiceOrderItemWithMainFeatureViewModel
+    {
+        //public ServiceOrderItem ServiceOrderItem { get; set; }
+        public string MainFeature { get; set; }
+    }
+}
